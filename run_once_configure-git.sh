97f88cdf6bd68c026bb7e8adbca458b7d5d0814b
Sam Weil <s.weil9391@gmail.com>
2026-04-04 20:34:30 +0000

    Add git aliases by default

--- run_once_configure-git.sh
+++ run_once_configure-git.sh
@@ -3,7 +3,7 @@
 git config --global init.defaultBranch main
 git config --global push.autoSetupRemote true
 git config --global core.editor vim
-
+git config --global --add include.path "~/.git_aliases"
 
 # Set up Delta
 git config --global core.pager delta
