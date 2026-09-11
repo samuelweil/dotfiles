8d7f31c5f0979035422645ac40221215b6599f6c
Sam Weil <sam@weil-labs.com>
2026-09-11 16:01:05 -0700

    Update neovim configuration
    - Set up Octo for code review
    - Disable warnings for markdown files

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Octo
+vim.keymap.set("n", "<leader>oi", "<cmd>Octo issue list<cr>", { desc = "Octo Issue List" })
+vim.keymap.set("n", "<leader>op", "<cmd>Octo pr list<cr>", { desc = "Octo PR List" })
+vim.keymap.set("n", "<leader>os", "<cmd>Octo pr search<cr>", { desc = "Octo PR Search" })
