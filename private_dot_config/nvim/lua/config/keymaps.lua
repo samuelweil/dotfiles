9ba19c872d7d70338a6eec57c31bdce3bab5da7c
Sam Weil <sam@weil-labs.com>
2026-09-15 14:28:31 -0700

    Center when paging up/down in vim

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Center cursor after half-page scroll
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down half page (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up half page (centered)" })
+
 -- Octo
 vim.keymap.set("n", "<leader>oi", "<cmd>Octo issue list<cr>", { desc = "Octo Issue List" })
 vim.keymap.set("n", "<leader>op", "<cmd>Octo pr list<cr>", { desc = "Octo PR List" })
